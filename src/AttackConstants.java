a76d9e1c334bf8b4034a2f3920040fabd9d48b53
crisferlop <crisrivlop@gmail.com>
2014-05-27 21:19:59 -0600

    Documentation Class Diagram

---
+++ src/AttackConstants.java
@@ -0,0 +1,8 @@
+
+public class AttackConstants {
+	
+	public static String PATH_ATTACK_1 = "gallery/attack1.png";
+	public static String PATH_ATTACK_2 = "gallery/attack2.png";
+	public static String PATH_ATTACK_3 = "gallery/attack3.png";
+
+}
---
+++ src/Renderizable.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+
+public abstract class Renderizable {
+	private int x,y,height,width;
+	private Rectangle rectangle;
+	protected static Graphics g;
+
+	public abstract void update();
+	public abstract void render();
+	
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public int getWidth(){
+		return width;
+	}
+	
+	public Rectangle getRectangle(){
+		return rectangle;
+	}
+	public boolean colision(Renderizable prenderizable){
+		return rectangle.intersects(prenderizable.getRectangle());
+	}
+}
